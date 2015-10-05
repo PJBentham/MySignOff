6fde189876b9fe10f8855d15e9c1721b1acbc137
Paul Bentham <pjbentham@gmail.com>
2015-10-05 21:43:25 +0100

    models updated, tables set up for projects, user page now shows whatever projects are set up for that user

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import (Flask, g, render_template, flash, redirect, url_for)
 from flask.ext.bcrypt import check_password_hash
-from flask.ext.login import LoginManager, login_user, logout_user, login_required
+from flask.ext.login import (LoginManager, login_user, logout_user,
+							login_required, current_user)
+from flask_debugtoolbar import DebugToolbarExtension
 
 import forms
 import models
@@ -11,6 +13,9 @@ HOST = '0.0.0.0'
 
 app = Flask(__name__)
 app.secret_key = 'kjheruieveqw7863414756___$%^'
+app.debug = True
+toolbar = DebugToolbarExtension(app)
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -23,11 +28,14 @@ def load_user(userid):
 	except models.DoesNotExist:
 		return None
 
+
 @app.before_request
 def before_request():
 	"""Connect to the database before each request"""
 	g.db = models.DATABASE
 	g.db.connect()
+	g.user = current_user
+
 
 @app.after_request
 def after_request(response):
@@ -35,23 +43,31 @@ def after_request(response):
 	g.db.close()
 	return response
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	form = forms.LoginForm()
 	form2 = forms.InterestedForm()
 	if form.validate_on_submit():
 		try:
-			user = models.User.get(models.User.email == form.username.data)
+			user = models.User.get(models.User.username == form.username.data)
 		except models.DoesNotExist:
 			flash("Your email or password doesn't match!", "error")
 		else:
 			if check_password_hash(user.password, form.password.data):
 				login_user(user)
 				flash("You've been logged in", "success")
-				return redirect(url_for('profile', username=form.username.data))
+				return redirect(url_for('profile', name=g.user.username))
 			else:
 				flash("Your email or password doesn't match!", "error")
 	return render_template('home.html', form=form, form2=form2)
+
+@app.route('/User/<name>', methods=['GET', 'POST'])
+@login_required
+def profile(name):
+	projects = models.Project.get_projects(name)
+	return render_template('user.html', username=name, projects=projects)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -71,18 +87,13 @@ def logout():
 	logout_user()
 	return redirect(url_for('index'))
 
-@app.route('/User/<username>', methods=['GET', 'POST'])
-@login_required
-def profile(username):
-	return render_template('user.html', username=username)
-
-@app.route('/User/<username>/<project>', methods=['GET', 'POST'])
-@login_required
-def project(username, project):
-	if models.User.position == "manager":
-		return render_template('table.html', username=username, project=project)
-	else:
-		return render_template('form.html', username=username=, project=project)
+# @app.route('/User/<username>/<project>', methods=['GET', 'POST'])
+# @login_required
+# def project(username, project):
+# 	if models.User.position == "manager":
+# 		return render_template('table.html', username=username, project=project)
+# 	else:
+# 		return render_template('form.html', username=username, project=project)
 
 
 if __name__ == '__main__':
@@ -92,8 +103,17 @@ if __name__ == '__main__':
 			username='paulbentham',
 			email='pjbentham@gmail.com',
 			password='admin',
-			admin=True
+			admin=True,
+			position="manager",
 			)
+		models.Project.create_project(
+			username='paulbentham',
+			projectname='freezer'
+		)
+		models.Project.create_project(
+			username='paulbentham',
+			projectname='locker'
+		)
 	except ValueError:
 		pass
 	app.run(debug=DEBUG, host=HOST, port=PORT)
--- models.py
+++ models.py
@@ -12,22 +12,61 @@ class User(UserMixin, Model):
 	password = CharField(max_length=100)
 	joined_at = DateTimeField(default=datetime.datetime.now)
 	is_admin = BooleanField(default=False)
+	position = CharField()
 
 	class Meta:
 		database = DATABASE
 
 	@classmethod
-	def create_user(cls, username, email, password, admin=False):
+	def create_user(cls, username, email, password, admin, position):
 		try:
 			cls.create(
 				username=username,
 				email=email,
 				password=generate_password_hash(password),
-				is_admin=admin)
+				is_admin=admin,
+				position=position)
 		except IntegrityError:
 			raise ValueError("User already exists")
 
+class Project(Model):
+	username = CharField()
+	projectname = CharField()
+
+	class Meta:
+		database = DATABASE
+
+	@classmethod
+	def get_projects(self, name):
+		projects = (Project
+				.select()
+				.where(Project.username == name))
+		return projects
+
+				# .join(UserProject)
+				# .join(User)
+
+
+	@classmethod
+	def create_project(cls, username, projectname):
+		try:
+			cls.create(
+				username=username,
+				projectname=projectname)
+		except IntegrityError:
+			raise ValueError("User already exists")
+
+class UserProject(Model):
+	user = ForeignKeyField(User)
+	project = ForeignKeyField(Project)
+
+	class Meta:
+		database = DATABASE
+		indexes = (
+			(('user', 'project'), True)
+		)
+
 def initialize():
 	DATABASE.connect()
-	DATABASE.create_tables([User], safe=True)
+	DATABASE.create_tables([User, Project, UserProject], safe=True)
 	DATABASE.close()
